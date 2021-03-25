4ee526b42a45a57423142ba2dabae618ed9522b1
kuro-kuroite <26164343+kuro-kuroite@users.noreply.github.com>
2021-03-25 15:52:07 +0000

    test(blogpost): :rotating_light: add test for blogPost with Jest, Storybook, and Cypress

---
+++ src/components/Index/BlogInfo/BlogInfo.stories.tsx
@@ -0,0 +1,23 @@
+import type { Meta, Story } from '@storybook/react';
+import React, { PropsWithChildren } from 'react';
+
+import {
+  PureBlogInfo as PureBlogInfoComponent,
+  PureProps as BlogInfoPureProps,
+} from './BlogInfo';
+
+export default {
+  component: PureBlogInfoComponent,
+  title: 'components/Index/BlogInfo',
+} as Meta;
+
+const Template: Story<PropsWithChildren<BlogInfoPureProps>> = (args) => (
+  <PureBlogInfoComponent {...args} />
+);
+
+export const BlogInfo = Template.bind({});
+BlogInfo.args = {
+  createdAt: '08/22/2020',
+  tags: ['React', 'TypeScript', 'Gatsby', 'TSX'],
+  updatedAt: '08/24/2020',
+};
