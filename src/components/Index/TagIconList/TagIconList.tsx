5ba11842bac55568f807dd58375848198b24feef
kuro-kuroite <26164343+kuro-kuroite@users.noreply.github.com>
2021-03-26 05:46:47 +0000

    feat(remark,blogpost): :sparkles: add blogPost with remark, TSX, PostCSS, Tailwind, and I18n

---
+++ src/components/Index/TagIconList/TagIconList.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { FaTag } from 'react-icons/fa';
+
+import { useT } from '../../../utils/i18n';
+import { Props as TagProps, TagIcon } from '../TagIcon/TagIcon';
+import styles from './TagIconList.module.css';
+
+export const PureTagIconList: React.VFC<PureProps> = ({ tags }) => {
+  const t = useT('components/index/tagIconList');
+
+  return (
+    <nav aria-label={t('タグナビゲーション')} className={styles.TagIconList}>
+      <FaTag className={styles.WithIcon} />
+      <ul className={styles.IconList}>
+        {tags.map((tag) => {
+          return (
+            <li key={tag}>
+              <TagIcon tag={tag} />
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+};
+
+export const TagIconList: React.VFC<Props> = ({ tags }) => {
+  return <PureTagIconList tags={tags} />;
+};
+
+export type PureProps = Props;
+
+export type Props = {
+  tags: {
+    [P in 'tag']: TagProps[P];
+  }['tag'][];
+};
